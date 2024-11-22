6c6c4e4b54dd89b9877b224eb95c2c103101ee1f
Radia731 <radiajjr17@gmail.com>
2024-11-22 07:48:39 -0500

    initial commit. header component created

---
+++ Ecom Project/ecomProject/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Components/header/Header';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+ 
+    <div>
+       <Header/>
+    </div>
+
+  
+    </>
+  )
+}
+
+export default App
