81fa2f402b1ebd84b06d251c35300b1541135f2c
Radia731 <radiajjr17@gmail.com>
2024-11-23 14:55:40 -0500

    created body componenet

---
+++ groupOneProject/ecomProject/src/Components/body/Body.jsx
@@ -0,0 +1,23 @@
+import './Body.css'
+ 
+function Body() {
+
+    return(
+        <>
+            <div className="body-bg">
+
+             <div className="body-description">
+                <p>FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+                <h3> Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</h3>
+             </div>
+
+             <div className="body-btn">
+             <button className="button-arounder">Sign Up</button>
+             </div>
+
+            </div>
+
+        </>
+    )
+}
+export default Body;
