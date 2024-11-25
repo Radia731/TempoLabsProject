38d4b7ea3616cc16ebb58d95168aa87ea99b9f41
Radia731 <radiajjr17@gmail.com>
2024-11-25 03:15:21 -0500

    brand div added and sparkling div adjusted

--- groupOneProject/ecomProject/src/Components/body/Body.jsx
+++ groupOneProject/ecomProject/src/Components/body/Body.jsx
@@ -1,54 +1,52 @@
-import './Body.css'
- 
+import './Body.css';
+
 function Body() {
-
-return(
-     <>
-        <div className="body-bg">
-            <div className="sprinklebg">
-
-                    <div className="sprinkle-1" > </div>
-                  <div className="sprinkle-2"> </div>
-
-                     <div className="body-description">
-                         <p>FIND CLOTHES THAT MATCHES YOUR STYLE</p>
-                        <h3> Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</h3>
-                     </div>
-
-                     <div className="body-btn">
-                        <button className="button-arounder">Sign Up</button>
-                    </div>
- 
-                <div className="info-barContainer">
-                      <div className="info-item">
-                        <p className="info-number">200+</p>
-                        <p className="info-label">International Brands</p>
-                      </div>
-                   
-                    <div className="divider"></div>
-                    
-                    <div className="info-item">
-                        <p className="info-number">2,000+</p>
-                        <p className="info-label">High-Quality Products</p>
-                    </div>
-                    
-                    <div className="divider"></div>
-                  
-                    <div className="info-item">
-                        <p className="info-number">30,000+</p>
-                        <p className="info-label">Happy Customers</p>
-                    </div>
-                        
-                     </div>
-                </div>
-
-
-
-
-
+    return (
+        <>
+            <div className="body-bg">
+                
+            <div class="sprinkle-container">
+                    <div class="sprinkle-1"></div>
+                    <div class="sprinkle-2"></div>
             </div>
 
+                    <div className="body-description">
+                        <p>FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+                        <h3>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</h3>
+                    </div>
+
+                    <div className="body-btn">
+                        <button className="button-arounder">Sign Up</button>
+                    </div>
+
+                    <div className="info-barContainer">
+                        <div className="info-item">
+                            <p className="info-number">200+</p>
+                            <p className="info-label">International Brands</p>
+                        </div>
+                        <div className="divider"></div>
+                        <div className="info-item">
+                            <p className="info-number">2,000+</p>
+                            <p className="info-label">High-Quality Products</p>
+                        </div>
+                        <div className="divider"></div>
+                        <div className="info-item">
+                            <p className="info-number">30,000+</p>
+                            <p className="info-label">Happy Customers</p>
+                        </div>
+                    </div>
+                </div>
+            
+
+            <div className="brand-bar"> 
+                    <li className="versace-logo"></li>
+                    <li className="zara-logo"></li>
+                    <li className="gucci-logo"></li>
+                    <li className="prada-logo"></li>
+                    <li className="ck-logo"></li>
+            </div>
         </>
-    )
+    );
 }
+
 export default Body;
